c6536f720184e9be472b7ee6065f94dcc16462ea
Christopher Burrows <christopherburrows@sphonic.com>
2023-05-02 21:54:03 +0100

    Added delete operations

--- api/shopping.go
+++ api/shopping.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"path"
+	"strconv"
 	"time"
 
 	"github.com/verminio/shopfam/shopping"
@@ -82,5 +84,24 @@ func ListItems(service *shopping.ItemService) http.HandlerFunc {
 			http.Error(w, "Unexpected error", http.StatusInternalServerError)
 			return
 		}
+	})
+}
+
+func DeleteItem(service *shopping.ItemService) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		param := path.Base(req.URL.Path)
+		id, err := strconv.Atoi(param)
+		if err != nil {
+			http.Error(w, "Bad request", http.StatusBadRequest)
+			return
+		}
+
+		if err = service.Delete(shopping.ItemId(id)); err != nil {
+			log.Printf("Unexpected error: %s", err)
+			http.Error(w, "Unexpected error", http.StatusInternalServerError)
+			return
+		}
+
+		http.Error(w, "", http.StatusNoContent)
 	})
 }
--- shopping/service.go
+++ shopping/service.go
@@ -7,7 +7,7 @@ type ItemService struct {
 func (s *ItemService) Upsert(id *ItemId, i item) (res *ItemId, err error) {
 	if id != nil {
 		res = id
-		err = s.repository.UpdateItem(id, i)
+		err = s.repository.UpdateItem(*id, i)
 	} else {
 		res, err = s.repository.SaveItem(i)
 	}
@@ -17,6 +17,10 @@ func (s *ItemService) Upsert(id *ItemId, i item) (res *ItemId, err error) {
 
 func (s *ItemService) List() (items, error) {
 	return s.repository.ListItems()
+}
+
+func (s *ItemService) Delete(id ItemId) error {
+	return s.repository.DeleteItem(id)
 }
 
 func NewItemService(r Repository) *ItemService {
